b4777c43435f7893afe08feacde8edc9f6b228b0
Ian Lau <ianlau@IandeMacBook-Pro.local>
2024-06-11 11:56:42 +0800

    Initail commit

---
+++ src/main/java/com/ian/easychores/EasyChoresApplication.java
@@ -0,0 +1,13 @@
+package com.ian.easychores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyChoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyChoresApplication.class, args);
+	}
+
+}
