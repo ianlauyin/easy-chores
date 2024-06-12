70db257cafe3c73493fec9445d4af2d6f408969a
ianlau <ianlauyin@gmail.com>
2024-06-12 10:54:47 +0800

    Updated with docker-compose.yml

--- src/main/java/com/ian/easychores/models/User.java
+++ src/main/java/com/ian/easychores/models/User.java
@@ -1,4 +1,22 @@
 package com.ian.easychores.models;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true)
+    private Integer id;
+
+    @Column(unique = true,nullable = false,updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String hashedPassword;
+
+    @Column(nullable = false)
+    private String userName;
 }
